d2dbe453d5628f9bb31c47c3a88290ccaa48568e
Binyu <binyu1231@gmail.com>
2024-05-29 09:37:35 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- font-sans -->
+  <main>
+    <RouterView />
+  </main>
+</template>
+<script setup lang="ts">
+// @ts-ignore
+import AOS from 'aos'
+import { toggleDark, useSystemAccess } from '~/composables'
+import { useRouter } from 'vue-router'
+
+const { isSignin } = useSystemAccess()
+
+const router = useRouter()
+// const route = useRoute()
+
+onMounted(() => {
+  toggleDark(true)
+  AOS.init({
+    once: true,
+    disable: 'phone',
+    duration: 500,
+    easing: 'ease-out-cubic',
+  })
+
+  if (!isSignin.value) {
+    router.replace('/access/signin')
+  }
+
+})
+
+
+
+</script>
---
+++ src/pages/[...all].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const router = useRouter()
+const route = useRoute()
+function handleBackHome() {
+  router.replace('/dashboard')
+}
+
+onBeforeMount(() => {
+  if (route.path === '/') handleBackHome()
+})
+
+
+
+</script>
+<template>
+  <div class="fixed inset-0 flex flex-col items-center justify-center gap-6">
+    <GradientColorText class="text-2xl font-800">404 Not Found</GradientColorText>
+
+    <GlassButton @click="handleBackHome">Go Home</GlassButton>
+  </div>
+</template>
